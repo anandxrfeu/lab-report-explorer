dac41b26c7519381cb37e2c722232b552b456cee
Anand Chelvan <anandtchelvan.dev@gmail.com>
2022-12-07 06:25:16 -0300

    Feature: Search exams by test results

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useLocation } from 'react-router-dom';
 import axios from "axios"
 import Navbar from "../components/layout/Navbar";
@@ -7,7 +7,15 @@ const Search = () => {
 
     const [loading, setLoading] = useState(true)
     const [files, setFiles] = useState([])
+    const [filesCopy, setFilesCopy] = useState([])
+
     const location = useLocation()
+
+    let folderId = 0
+    const mimeType = "application/pdf"
+    const GDRIVE_FILES_END_POINT_URI = process.env.REACT_APP_GDRIVE_API_END_POINT
+
+    const searchTerm = useRef("")
 
     useEffect( ()=>{
 
@@ -30,7 +38,7 @@ const Search = () => {
         async function fetchData (params) {
             try{
                 const access_token = params["access_token"]
-                const GDRIVE_FILES_END_POINT_URI = process.env.REACT_APP_GDRIVE_API_END_POINT
+
                 // get file id for the health folder
                 const result = await axios.get(GDRIVE_FILES_END_POINT_URI,{
                        params:{
@@ -44,10 +52,10 @@ const Search = () => {
                     }
                 )
                 const folder = result.data.files.filter(x => x.name === process.env.REACT_APP_GDRIVE_FOLDER_NAME);
-                const folderId = folder.length? folder[0].id : 0;
+                folderId = folder.length? folder[0].id : 0;
+                localStorage.setItem("folderId", folderId)
 
                 // get all pdf files in above folder
-                const mimeType = "application/pdf"
                 const response = await axios.get(GDRIVE_FILES_END_POINT_URI,{
                     params:{
                         q: `'${folderId}' in parents and mimeType:'${mimeType}'`,
@@ -60,6 +68,7 @@ const Search = () => {
                 })
                 setLoading(false)
                 setFiles(response.data.files)
+                setFilesCopy(response.data.files)
             }catch(err){
                 console.log("err> ", err)
                 if(err.response.status === 401){
@@ -71,6 +80,54 @@ const Search = () => {
 
     }, [])
 
+
+    const onSearchHandler = async (e) => {
+        e.preventDefault()
+        const params = JSON.parse(localStorage.getItem("oauth2-params"))
+        const access_token = params.access_token
+        //console.log("Access token > ",params.access_token)
+        //console.log("searchTerm  ", searchTerm.current.value)
+        folderId = localStorage.getItem("folderId")
+        //console.log("folderId", folderId)
+
+        if (searchTerm === "") return;
+        
+        try{
+        
+            // get all pdf files in above folder
+            const response = await axios.get(GDRIVE_FILES_END_POINT_URI,{
+            params:{
+                q: `'${folderId}' in parents and mimeType:'${mimeType}' and fullText contains '${searchTerm.current.value}'`,
+                fields: 'files(id,name,mimeType,webContentLink ,size)',
+                spaces: 'drive',
+              },
+            headers: {
+                authorization: `Bearer ${access_token}`
+            }
+            })
+            //console.log("response.data.files ",response)
+            console.log("response.data.files ",response.data.files)
+            // setLoading(false)
+//            setFiles(response.data.files)
+            setFilesCopy(response.data.files)
+
+        }catch(err){
+            console.log("err> ", err)
+            if(err.response.status === 401){
+                console.log("Please sign in again!")
+            }
+
+        }
+
+    }
+
+    const onChangeHandler = (e) =>{
+        if(e.target.value === ""){
+            setFilesCopy(files)
+        }
+    }
+
+
     if(loading){
         return <p>Searching...</p>
     }
@@ -78,9 +135,19 @@ const Search = () => {
     return(
         <div>
             <Navbar />
-            {files && files.length > 0 && (
-                 files.map(file => <p key={file.id}>{file.name} <a href={file.webContentLink}>download</a></p>)
+            <div className="search">
+                <input type="search" ref={searchTerm} onChange={onChangeHandler}/>
+                <button onClick={onSearchHandler}>Search</button>
+            </div>
+            <div className="file_section">
+                {filesCopy && filesCopy.length > 0 && (
+                    filesCopy.map(file => (
+                        <div>
+                            <p key={file.id}>{file.name} <a href={file.webContentLink}>download</a></p>
+                        </div>
+                        ))
             )}
+            </div>
         </div>
         )
 
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,6 +1,40 @@
+import Navbar from "../components/layout/Navbar";
+import { useState } from "react"
+
+
+
 const Upload = () => {
 
-    return(<p>Upload Page</p>)
+    const [imageUrl, setImageUrl] = useState("")
+
+
+    const handleFileUpload = async (e) =>{
+        const uploadData = new FormData()
+        uploadData.append("imageUrl", e.target.files[0])
+        try{
+            console.log("uploadData ", uploadData)
+            //const data = await apiService.uploadFile(uploadData)
+            //setImageUrl(data.filePath)
+        }catch(err){
+            console.log(err)
+        }
+      }
+
+    return(
+        <div>
+            <Navbar />
+            <div className="upload_section">
+            <input
+                type="file"
+                name="imageUrl"
+                onChange={handleFileUpload}
+            />
+            <button
+                disabled={imageUrl === ""}
+                className="profile-form-controls_btn badge-pill">Save</button>
+            </div>
+        </div>
+    )
 
 }
 
